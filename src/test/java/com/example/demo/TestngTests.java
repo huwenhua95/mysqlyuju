21a9a937b66b377bfb10f83fea4dbd8bab5e168c
huwenhua95 <1751400385@qq.com>
2019-08-18 16:45:19 +0800

    tijiao

--- src/test/java/com/example/demo/TestngTests.java
+++ src/test/java/com/example/demo/TestngTests.java
@@ -1,15 +1,28 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestngTests {
     @Test
     public void testCase1() {
-        System.out.println();
+       System.out.println("这是测试用例1 ");
     }
+    @Test
+    public void testCase2() {
+        System.out.println("这是测试用例2");
+    }
+    @Before
+    public void beforeMethod() {
+        System.out.println(" BeforeMethod这是测试方法之前运行的 3");
+    }
+
+    @After
+    public void afterMethod() {
+        System.out.println("AfterMethod这是测试方法之后运行的 5");
+    }
+
 }
+
